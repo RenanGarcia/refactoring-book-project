e2e9d681250bd840d154c3d7e5aaa1086d096904
Renan Garcia <renan.nobregarcia@gmail.com>
2020-09-12 23:14:01 -0300

    Procedure: Replace Temp with Query

--- src/statment.js
+++ src/statment.js
@@ -10,33 +10,37 @@ function statment(invoice, plays) {
   }).format;
 
   function amountFor(aPerformance, play) {
-  let result = 0;
+    let result = 0;
 
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
+    switch (play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
 
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
 
-    default:
-      throw new Error(`unknown type: ${play.type}`);
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+
+    return result;
   }
 
-  return result;
-}
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
 
     let thisAmount = amountFor(perf, play);
 
