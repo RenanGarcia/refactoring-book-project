3b7a66e73997982a56e3f2bec662841d361df744
Renan Garcia <renan.nobregarcia@gmail.com>
2020-09-12 23:57:17 -0300

    Procedure: Replace Temp with Query

--- src/statment.js
+++ src/statment.js
@@ -60,10 +60,8 @@ function statment(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = totalVolumeCredits();
-
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 }
 
