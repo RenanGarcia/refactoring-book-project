2ffc232af63ffedcf695de58a633040461981ea8
Renan Garcia <renan.nobregarcia@gmail.com>
2020-09-13 02:47:43 -0300

    Added createStatmentData spec

--- src/statment.spec.js
+++ src/statment.spec.js
@@ -1,7 +1,46 @@
-const { statment, htmlStatment } = require("./statment.js");
+const createStatmentData = require("./createStatmentData");
+const { statment, htmlStatment } = require("./statment");
 
 const plays = require("./plays.json");
 const invoices = require("./invoices.json");
+
+const expectedStatmentData = {
+  customer: "BigCo",
+  performances: [
+    {
+      amount: 65000,
+      audience: 55,
+      play: {
+        name: "Hamlet",
+        type: "tragedy",
+      },
+      playID: "hamlet",
+      volumeCredits: 25,
+    },
+    {
+      amount: 58000,
+      audience: 35,
+      play: {
+        name: "As You Like It",
+        type: "comedy",
+      },
+      playID: "as-like",
+      volumeCredits: 12,
+    },
+    {
+      amount: 50000,
+      audience: 40,
+      play: {
+        name: "Othello",
+        type: "tragedy",
+      },
+      playID: "othello",
+      volumeCredits: 10,
+    },
+  ],
+  totalAmount: 173000,
+  totalVolumeCredits: 47,
+};
 
 const expectedTextStatment = `\
 Statment for BigCo
@@ -25,6 +64,12 @@ const expectedHtmlStatment = `\
 `;
 
 describe("Statment", () => {
+  it("should create statment data", () => {
+    expect(createStatmentData(invoices[0], plays)).toEqual(
+      expectedStatmentData
+    );
+  });
+
   it("should render BigCo text statment", () => {
     expect(statment(invoices[0], plays)).toEqual(expectedTextStatment);
   });
